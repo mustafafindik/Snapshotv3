e282173449975d32d78ea31f46457c50dbde0d88
mustafafindik <mustafa.findik3@gmail.com>
2019-07-31 17:49:20 +0300

    Indicator

--- app/src/main/java/findik/mustafa/snapshot/views/SnapTabsView.java
+++ app/src/main/java/findik/mustafa/snapshot/views/SnapTabsView.java
@@ -8,8 +8,10 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -19,14 +21,18 @@ public class SnapTabsView extends FrameLayout implements ViewPager.OnPageChangeL
 
     private ImageView mCapturePhotoBtn;
     private ImageView mChatBtn;
-
-
-
     private ImageView mStoryBtn;
+    private View mIndicator;
+
+    private float mCenterTranslationY;
+    private float mTranslationY;
+    private float mIndicatorTranslationX;
+    private float mEndViewsTranslationX;
 
     private int mCenterColor;
     private int mOffsetColor;
     private ArgbEvaluator mColorEval;
+
 
     public SnapTabsView(@NonNull Context context) {
         this(context, null);
@@ -44,13 +50,39 @@ public class SnapTabsView extends FrameLayout implements ViewPager.OnPageChangeL
 
     private void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.view_snap_tabs, this, true);
-        mCapturePhotoBtn =   findViewById(R.id.capture_photo_btn);
-        mChatBtn =  findViewById(R.id.chat_btn);
-        mStoryBtn =   findViewById(R.id.story_btn);
+
+        mCapturePhotoBtn =   findViewById(R.id.capture_photo_btn);  //center
+        mChatBtn =  findViewById(R.id.chat_btn); // satrt
+        mStoryBtn =   findViewById(R.id.story_btn); // end
 
         mCenterColor = ContextCompat.getColor(getContext(), R.color.white);
         mOffsetColor = ContextCompat.getColor(getContext(), R.color.light_purple);
         mColorEval = new ArgbEvaluator();
+
+        mCenterColor = ContextCompat.getColor(getContext(), R.color.white);
+        mOffsetColor = ContextCompat.getColor(getContext(), R.color.dark_grey);
+
+        final int centerPadding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 122,
+                getContext().getResources().getDisplayMetrics());
+
+
+        mIndicator = findViewById(R.id.vst_indicator);
+
+        mCapturePhotoBtn.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                mCenterTranslationY = getHeight() - mCapturePhotoBtn.getBottom();
+                float distanceBetween = mCapturePhotoBtn.getX() - mChatBtn.getX();
+                mEndViewsTranslationX = distanceBetween - centerPadding;
+                mTranslationY =  getHeight() - mChatBtn.getBottom();
+                mIndicatorTranslationX = centerPadding;
+
+                mCapturePhotoBtn.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+
+        });
+
     }
 
     public ImageView getmCapturePhotoBtn() {
@@ -108,13 +140,48 @@ public class SnapTabsView extends FrameLayout implements ViewPager.OnPageChangeL
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
         if(position == 0) {
-            setColor(positionOffset);
+            setUpViews(
+                    1 - positionOffset,
+                    .7f + (positionOffset * .3f),
+                    (1 - positionOffset) * mCenterTranslationY,
+                    -mIndicatorTranslationX * (1-positionOffset)
+            );
         }
         else if(position == 1) {
-            setColor(1-positionOffset);
-
+            setUpViews(
+                    positionOffset,
+                    .7f + ((1 - positionOffset) * .3f),
+                    positionOffset * mCenterTranslationY,
+                    mIndicatorTranslationX * positionOffset
+            );
         }
     }
+
+    private void setUpViews(float fractionFromCenter, float centerScale, float centerTransY, float indicatorTransX) {
+        mIndicator.setAlpha(fractionFromCenter);
+        mIndicator.setScaleX(fractionFromCenter);
+
+        mChatBtn.setTranslationX(-mEndViewsTranslationX * fractionFromCenter);
+        mStoryBtn.setTranslationX(mEndViewsTranslationX * fractionFromCenter);
+
+        mCapturePhotoBtn.setScaleX(centerScale);
+        mCapturePhotoBtn.setScaleY(centerScale);
+
+        mCapturePhotoBtn.setTranslationY(centerTransY);
+        mChatBtn.setTranslationY(mTranslationY-50);
+        mStoryBtn.setTranslationY(mTranslationY-50);
+
+
+
+        mIndicator.setTranslationX(indicatorTransX);
+
+
+        int color = (int) mColorEval.evaluate(fractionFromCenter, mCenterColor, mOffsetColor);
+        mChatBtn.setColorFilter(color);
+        mStoryBtn.setColorFilter(color);
+        mCapturePhotoBtn.setColorFilter(color);
+    }
+
 
     @Override
     public void onPageSelected(int position) {
